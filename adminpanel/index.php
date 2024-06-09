e42a5e48d5d125d6190b186ce3e819f20cb4bf38
khakhaaa <rakha_adrian_32rpl@student.smktelkom-mlg.sch.id>
2024-06-09 19:18:45 +0700

    add navbar

--- adminpanel/index.php
+++ adminpanel/index.php
@@ -7,9 +7,13 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Home</title>
+    <title>Dashboard</title>
+    <link rel="stylesheet" href="../bootstrap/css/bootstrap.min.css">
 </head>
 <body>
+    <?php 
+    require "navbar.php";
+    ?>
     <h2>Selamat Datang <?php echo $_SESSION['username']?></h2>
 </body>
 </html>
---
+++ adminpanel/navbar.php
@@ -0,0 +1,23 @@
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark p-2">
+    <div class="container-fluid">
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0 place-items-center">
+                <li class="nav-item me-4">
+                    <a class="nav-link" href="../adminpanel">Home</a>
+                </li>
+                <li class="nav-item me-4">
+                    <a class="nav-link" href="kategori.php">Kategori</a>
+                </li>
+                <li class="nav-item me-4">
+                    <a class="nav-link" href="produk.php">Produk</a>
+                </li>
+            </ul>
+            <a class="nav-link" href="logout.php">
+                <button class="btn btn-danger" type="submit">Logout</button>
+            </a>
+        </div>
+    </div>
+</nav>
